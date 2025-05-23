8e99c5a350943fc418e82e5b3a88bc10cbb4251b
Aleksundraaa <ladycaxap@gmail.com>
2025-05-23 22:03:23 +0500

    класс CanvasManager в отдельный файл

--- main.py
+++ main.py
@@ -2,36 +2,6 @@ import tkinter as tk
 from tkinter import filedialog, colorchooser, messagebox, simpledialog
 from PIL import Image, ImageDraw, ImageTk, ImageColor
 
-
-class CanvasManager:
-    def __init__(self, root, width, height, bg_color):
-        self.width = width
-        self.height = height
-        self.bg_color = bg_color
-
-        self.canvas = tk.Canvas(root, width=width, height=height, bg=bg_color)
-        self.canvas.pack()
-
-        self.image = Image.new("RGB", (width, height), bg_color)
-        self.draw = ImageDraw.Draw(self.image)
-        self.photo_image = None
-
-    def update_canvas(self):
-        self.canvas.delete("all")
-        self.photo_image = ImageTk.PhotoImage(self.image)
-        self.canvas.create_image(0, 0, image=self.photo_image, anchor=tk.NW)
-
-    def resize_canvas(self, width, height):
-        old_image = self.image.copy()
-        self.width = width
-        self.height = height
-        self.image = Image.new("RGB", (width, height), self.bg_color)
-        self.image.paste(old_image, (0, 0))
-        self.draw = ImageDraw.Draw(self.image)
-        self.canvas.config(width=width, height=height)
-        self.update_canvas()
-
-
 class SelectionManager:
     def __init__(self, canvas_manager):
         self.canvas_manager = canvas_manager
