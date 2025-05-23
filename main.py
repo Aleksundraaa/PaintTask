a9a0b1df11c70bf2076dbd57dc0ec167e9f7adff
Aleksundraaa <ladycaxap@gmail.com>
2025-05-23 22:55:14 +0500

    в main.py добавлена возможность работать в пакетном режиме, помощь по --help и -h

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import tkinter as tk
 from MainPaint import MainPaint
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Графический растровый редактор")
+    parser.add_argument('--batch', action='store_true',
+                        help="Запуск в пакетном режиме")
+    args = parser.parse_args()
+    if args.batch:
+        print("Запуск в пакетном режиме...")
     root = tk.Tk()
     app = MainPaint(root)
     root.mainloop()
